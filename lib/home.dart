1b9076ad15727377f865a66da548312078dcb11d
Zainab Ramzan <zainab.rana806@gmail.com>
2021-05-20 12:05:24 +0500

    Edit texts in home

--- lib/home.dart
+++ lib/home.dart
@@ -9,6 +9,18 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container());
+        body: Column(
+      children: [
+        Container(
+          child: Text("My name is Saad"),
+        ),
+        Container(
+          child: Text("I am Flutter Developer"),
+        ),
+        Container(
+          child: Text("I am working in a Software house Webfix"),
+        ),
+      ],
+    ));
   }
 }

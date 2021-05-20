2936c88779c964311db35a63d541a2ee031fe531
Saad Saleem <saad.sal748@gmail.com>
2021-05-20 12:45:22 +0500

    Added Sample file

---
+++ lib/sample.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Smaple extends StatefulWidget {
+  @override
+  _SmapleState createState() => _SmapleState();
+}
+
+class _SmapleState extends State<Smaple> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
